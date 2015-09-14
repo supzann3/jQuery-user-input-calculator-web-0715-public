41ed8225f468a3ea302676a612cd21faa587ab1a
supzann3 <susan.hehe01@gmail.com>
2015-09-14 10:17:56 -0400

    Done.

--- js/calc.js
+++ js/calc.js
@@ -2,7 +2,26 @@
 $(function(){
 
   var calculator = new Calculator();
-
 });
 
+function Calculator () {
+  $('#equals').click(validate);
+}
+
+function validate() {
+  var $number1=$('#number1').val();
+  var $number2=$('#number2').val();
+  var $operator=$('#operation').val();
+
+  if (isNaN($number1) || isNaN($number2)) {
+    $('#result').text('Sorry, one of those is not a valid number!');
+  }
+  else if ($operator!= '+' && $operator!='-'&& $operator!= '*' && $operator!= '/') {
+    return $('#result').text('Sorry, not a valid operation!');
+  }
+  else{
+    var total=eval(parseInt($number1)+$operator+parseInt($number2));
+    $('#result').text(total);
+  }
+}
 // your code here!
